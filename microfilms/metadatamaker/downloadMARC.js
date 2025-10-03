008a282fcbf16357928a60aca22f5961ab102918
Doug Chestnut <dougchestnut@gmail.com>
2025-10-03 14:28:02 -0400

    fixes #23

--- microfilms/metadatamaker/downloadMARC.js
+++
@@ -1,6 +0,0 @@
-import { MicrofilmsMarcBuilder } from '../../shared/variants/microfilmsMarcBuilder.js';
-
-const builder = new MicrofilmsMarcBuilder();
-
-window.downloadMARC = builder.downloadMARC.bind(builder);
-window.downloadXML = builder.downloadXML.bind(builder);
