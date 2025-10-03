c88c5aa5f7176ac6a5a4822c5c5cf4e30c8e302b
Doug Chestnut <dougchestnut@gmail.com>
2025-10-03 00:05:22 -0400

    fixes #12

--- monoviaf/metadatamaker/downloadMARC.js
+++
@@ -1,6 +0,0 @@
-import { MonoviafMarcBuilder } from '../../shared/variants/monoviafMarcBuilder.js';
-
-const builder = new MonoviafMarcBuilder();
-
-window.downloadMARC = builder.downloadMARC.bind(builder);
-window.downloadXML = builder.downloadXML.bind(builder);
